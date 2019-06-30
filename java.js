bd5349d43cbf161c4c42dd5bbe735c5978e53bea
lo0oper <43717874+lo0oper@users.noreply.github.com>
2019-06-30 18:21:19 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+//document.getElementById("loginform").onclick=function () {changeclass('')};
+function changeclass(modalinvisible){
+    document.getElementById("loginmodal").style.display='block';
+    
+
+}
